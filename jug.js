b26c0cd7a53c0a3d2e980ee6ae484d0c1b9e3abf
admin <admin@pc-a157.local>
2016-12-15 03:27:58 +0900

    ローカルストレージ使った

--- jug.js
+++ jug.js
@@ -1,4 +1,3 @@
-//記録する技の選択
 
 //グラフの描写
 function draw(tmpdata, tmpdate){
@@ -26,6 +25,9 @@ data: {
   },
 options: {
    responsive: true,
+   scaleOverride : true,
+   scaleStepWidth : 5,
+   scaleStartValue : 0,
    scales: {
       xAxes: [{
          display: true,
@@ -42,7 +44,8 @@ options: {
             fontFamily: 'monospace',
             fontSize: 14
          },
-         ticks: {
+         ticks: {　//スケール
+           min:0,
             callback: function(value){
                return value+'回';
             }
@@ -53,9 +56,8 @@ options: {
 }
 )
 };
-var test = [20, 18, 22]
-var date = ["2016/12/1", "2016/12/2", "2016/12/3"]
-draw(test,date);
+
+var test = [];
 
 //日付取得
 var hiduke = new Date();
@@ -63,9 +65,57 @@ var year = hiduke.getFullYear();
 var month = hiduke.getMonth()+1;
 var day = hiduke.getDate();
 
+//カレンダー表示＆日付取得
+$(function() {
+  //ブラウザ再読み込み時にローカルストレージの中身を表示
+    if (localStorage.getItem('database')) {
+      //localStorage.removeItem('database');
+      test = JSON.parse(localStorage.getItem('database'));
+      console.log(test);
+      draw(test,array1);
+    }
+  $("#calender").datepicker({
+  onSelect: function(dateText, inst) {
+  //var date1 = dateText; // 08/13/2014の形
+  var date2 = $(this).datepicker( 'getDate' ); //　Wed Aug 13 2014 00:00:00 GMT+0900　の形
+  $("#date").text(date2.getMonth()+1);
+  datechange(date2);
+  }
+  });
+});
+
+//横軸：日付の配列作成
+var array1 = new Array(31);
+for (var i = 0; i < array1.length; i++) {
+  array1[i] = year + "/" + month + "/" + (i+1);
+}
+draw(test,array1);
+
 //記録ボタンクリック時のアクション
 function add(){
   test.push(document.kiroku.kaisuu.value);
-  date.push(year+"/"+month+"/"+day);
-  draw(test,date);
+  //localStorage.setItem('database',JSON.stringify(test)); //ローカルストレージに保存
+  //console.log(localStorage.getItem('database'));
+  draw(test,array1);
+}
+$('#addd').click(function(){
+  localStorage.setItem('database',JSON.stringify(test)); //ローカルストレージに保存
+  console.log(localStorage.getItem('database'));
+})
+
+//横軸グラフの日付変更
+function datechange(hiduke){
+  year = hiduke.getFullYear();
+  month = hiduke.getMonth()+1;
+  day = hiduke.getDate();
+  var loop;
+  array1=[];
+  if(month==2||month==4||month==6||month==9||month==11){
+    loop=30
+  }else{
+    loop=31
+  }
+  for (var i = 0; i < loop; i++) {
+    array1[i] = year + "/" + month + "/" + (i+1);
+  }
 }
